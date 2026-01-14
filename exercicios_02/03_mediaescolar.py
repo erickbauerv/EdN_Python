7fcca9ac52c3373f1e74e9cc6373f797154e7238
erickbauerv <contatoerickbauer@gmail.com>
2026-01-14 10:20:08 -0300

    Initial Commit

---
+++ exercicios_02/03_mediaescolar.py
@@ -0,0 +1,16 @@
+'''3 - Calculadora de Média Escolar
+Crie um programa que calcula a média escolar de um aluno. Use as seguintes notas:
+
+* Nota 1: 7.5
+* Nota 2: 8.0
+* Nota 3: 6.5
+O programa deve calcular a média e exibir todas as notas e o resultado final arredondando para duas casas decimais.'''
+
+nota_1 = 7.5
+nota_2 = 8.9
+nota_3 = 6.5
+media = (nota_1 + nota_2 + nota_3) / 3
+print(f"Nota 1: {nota_1}")
+print(f"Nota 2: {nota_2}")
+print(f"Nota 3: {nota_3}")
+print(f"Média final: {media:.2f}")
