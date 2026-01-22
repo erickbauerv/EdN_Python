bfbccf5b21f4342eaf4f88768f7008d62da43b43
erickbauerv <contatoerickbauer@gmail.com>
2026-01-22 17:05:13 -0300

    Exercícios 05 finalizados

--- exercicios_05/04_calcular_dias.py
+++ exercicios_05/04_calcular_dias.py
@@ -1,3 +1,24 @@
 '''
 4 - Crie um programa que calcule a quantos dias um individuo está vivo de acordo com a data do dia.
 '''
+from datetime import date
+
+
+print("--- Calculadora de Dias de Vida ---")
+
+try:
+    dia = int(input("Digite o dia do seu nascimento (DD): "))
+    mes = int(input("Digite o mês do seu nascimento (MM): "))
+    ano = int(input("Digite o ano do seu nascimento (AAAA): "))
+
+    data_nascimento = date(ano, mes, dia)
+    hoje = date.today()
+
+    diferenca = hoje - data_nascimento
+
+    if diferenca.days < 0:
+        print("\nVocê inseriu uma data no futuro.")
+    else:
+        print(f"\nVoce está vivo há {diferenca.days:,} dias!".replace(',', '.'))
+except ValueError:
+    print("\nErro: Por favor, insira números válidos para a data.")
