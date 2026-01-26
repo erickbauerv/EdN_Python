e0be93b82505ad727592e2112fb433684a9cbf4e
erickbauerv <contatoerickbauer@gmail.com>
2026-01-26 10:14:33 -0300

    Exercícios 6 finalizados

--- exercicios_06/02_buscarusuario.py
+++ exercicios_06/02_buscarusuario.py
@@ -3,3 +3,23 @@
 exibindo o nome, e-mail e país desse usuário, caso houver erro na conexão, mostre uma 
 mensagem de falha.
 '''
+
+import requests
+
+def criar_usuarios() -> dict:
+    url = "https://randomuser.me/api/"
+    resposta = requests.get(url, timeout=5)
+    resposta.raise_for_status()
+    return resposta.json()
+
+try:
+    dados = criar_usuarios()
+    usuario = dados["results"][0]
+    nome = f"{usuario["name"]["first"]} {usuario["name"]["last"]}"
+    email = usuario["email"]
+    pais = usuario["location"]["country"]
+    print(f"Nome: {nome}")
+    print(f"Email: {email}")
+    print(f"Pais: {pais}")
+except Exception:
+    print("Usuário não encontrado.")
